19b8250b9a04c93b3b1bdce07ea0726b2802d597
chenhenri@gmail.com <chenhenri@gmail.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2011-11-10 04:22:06 +0000

    fine tune example codes to add a picture in 2007 file format(.xlsx)
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@77 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- zssessentials/examples/src/org/zkoss/zssessentials/config/AddPictureComposer.java
+++ zssessentials/examples/src/org/zkoss/zssessentials/config/AddPictureComposer.java
@@ -11,31 +11,15 @@ Copyright (C) 2011 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zssessentials.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.zkoss.poi.ss.usermodel.ClientAnchor;
-import org.zkoss.poi.ss.usermodel.Workbook;
-import org.zkoss.poi.ss.usermodel.charts.ChartDataSource;
-import org.zkoss.poi.ss.usermodel.charts.ChartGrouping;
-import org.zkoss.poi.ss.usermodel.charts.ChartTextSource;
-import org.zkoss.poi.ss.usermodel.charts.ChartType;
-import org.zkoss.poi.ss.usermodel.charts.DataSources;
-import org.zkoss.poi.ss.usermodel.charts.LegendPosition;
-import org.zkoss.poi.ss.util.CellRangeAddress;
 import org.zkoss.poi.xssf.usermodel.XSSFClientAnchor;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFPieChartData;
 import org.zkoss.util.media.Media;
-import org.zkoss.zk.ui.Executions;
-import org.zkoss.zk.ui.event.MouseEvent;
 import org.zkoss.zk.ui.event.UploadEvent;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zss.model.Range;
 import org.zkoss.zss.model.Ranges;
 import org.zkoss.zss.model.Worksheet;
 import org.zkoss.zss.ui.Spreadsheet;
-import org.zkoss.zssex.license.LicenseContent;
-import org.zkoss.zssex.rt.RuntimeLicenseManager;
 import org.zkoss.zssex.util.PictureHelper;
 
 /**
@@ -56,5 +40,4 @@ public class AddPictureComposer extends GenericForwardComposer {
 		String fmt = media.getFormat();
 		rng.addPicture(anchor, image, PictureHelper.getPictureFormat(fmt));
 	}
-	
 }
