19e66ede9db0535afb34dbf7de4b6f21d032016a
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-06-13 09:45:44 +0000

    RegistrationComposer function completed.
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@158 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- getstarted/src/main/java/org/zkoss/simple/RegistrationComposer.java
+++ getstarted/src/main/java/org/zkoss/simple/RegistrationComposer.java
@@ -6,40 +6,69 @@ import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Checkbox;
-import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Radio;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
+@SuppressWarnings("serial")
 public class RegistrationComposer extends SelectorComposer<Window> {
 
-	@Wire("button[label='Submit']")
+	@Wire("#submitButton")
 	private Button submitButton;
 	
-	@Wire("textbox")
+	@Wire("#nameBox")
 	private Textbox nameBox;
-	@Wire("combobox")
-	private Combobox bloodTypeBox;
-	@Wire("datebox")
+	@Wire("#maleRadio")
+	private Radio maleRadio;
+	@Wire("#birthdayBox")
 	private Datebox birthdayBox;
 	
 	
-	@Wire("checkbox")
-	Checkbox acceptTermCheckbox;
+	@Wire("#acceptTermBox")
+	private Checkbox acceptTermCheckbox;
 	
-	@Listen("onCheck = checkbox")
+	@Listen("onCheck = #acceptTermBox")
 	public void changeSubmitStatus(){
 		if (acceptTermCheckbox.isChecked()){
 			submitButton.setDisabled(false);
+			submitButton.setImage("/images/submit.png");
 		}else{
 			submitButton.setDisabled(true);
+			submitButton.setImage("");
 		}
 	}
 	
-	@Listen("onClick = button[label='Reset']")
+	@Listen("onClick = #resetButton")
 	public void reset(){
-		nameBox.setValue("");
-		bloodTypeBox.setValue("");
-		birthdayBox.setValue(null);
+		//set raw value to avoid triggering constraint error message
+		nameBox.setRawValue("");
+		maleRadio.setChecked(true);
+		birthdayBox.setRawValue(null);
+		acceptTermCheckbox.setChecked(false);
+		submitButton.setDisabled(true);
+	}
+	
+	@Listen("onClick = #submitButton")
+	public void submit(){
+		if (!validateInput()){
+			return;
+		}
+		
+		Messagebox.show("Congratulation! "+nameBox.getValue()+". Your registration is success.");
+		reset();
+	}
+	
+	private boolean validateInput(){
+		if (nameBox.getValue().length()==0){
+			return false;
+		}
+		
+		if (birthdayBox.getValue()==null){
+			return false;
+		}
+	
+		return true;
 	}
 }
