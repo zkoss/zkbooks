cd53ecd1365acd4920bb947c4aaa62482b9ef1ca
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-06-28 02:53:57 +0000

    initial
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@177 04b67794-27a6-3c7a-43d9-00c53d7466cb

---
+++ zktutorial/src/main/java/org/zkoss/book/tutorial/DemoWindowComposer.java
@@ -0,0 +1,12 @@
+package org.zkoss.book.tutorial;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+public class DemoWindowComposer extends GenericForwardComposer {
+	
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		
+	}
+}
