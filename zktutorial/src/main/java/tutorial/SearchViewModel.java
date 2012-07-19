51092bb19e7f094b8ed2672022d0ee68e86ff4c0
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-07-19 09:00:05 +0000

    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@208 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- zktutorial/src/main/java/tutorial/SearchViewModel.java
+++ zktutorial/src/main/java/tutorial/SearchViewModel.java
@@ -10,31 +10,38 @@ public class SearchViewModel {
 	private BookService bookService = new BookServiceImpl();
 	
 	private List<Book> bookList;
-	
 	private String keyword;
 	
 	private Book selectedBook;
 	
 	
+	
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+
 	public List<Book> getBookList(){
 		return bookList;
 	}
 	
-	@NotifyChange("bookList")
-	@Command
-	public void search(){
-		bookList = bookService.search(keyword);
+	public void setBookList(List<Book> bookList) {
+		this.bookList = bookList;
 	}
-
-	public void setKeyword(String keyword) {
-		this.keyword = keyword;
-	}
-
-
+	
 	public void setSelectedBook(Book selectedBook) {
 		this.selectedBook = selectedBook;
 	}
 	public Book getSelectedBook() {
 		return selectedBook;
+	}
+
+	
+	@NotifyChange("bookList")
+	@Command
+	public void search(){
+		bookList = bookService.search(keyword);
 	}
 }
