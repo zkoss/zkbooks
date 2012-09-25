5824627d4343a617d3e4c3ee0bd53c75e86eb839
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-09-25 08:50:54 +0000

    add custom component example
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@282 04b67794-27a6-3c7a-43d9-00c53d7466cb

---
+++ devref/src/main/java/org/zkoss/reference/developer/mvvm/advance/EditableLabel.java
@@ -0,0 +1,51 @@
+package org.zkoss.reference.developer.mvvm.advance;
+
+import org.zkoss.zk.ui.HtmlMacroComponent;
+import org.zkoss.zk.ui.annotation.ComponentAnnotation;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Textbox;
+
+@ComponentAnnotation("value:@ZKBIND(ACCESS=both,SAVE_EVENT=onEdited)")
+public class EditableLabel extends HtmlMacroComponent{
+
+	private static final long serialVersionUID = 1L;
+
+	@Wire
+	Textbox textbox;
+	
+	@Wire
+	Label label;
+	
+	public EditableLabel(){
+		compose();
+	}
+	
+	public String getValue(){
+		return textbox.getValue();
+	}
+	
+	public void setValue(String value){
+		textbox.setValue(value);
+		label.setValue(value);
+	}
+
+	@Listen("onDoubleClick=#label")
+	public void doEditing(){
+		textbox.setVisible(true);
+		label.setVisible(false);
+		textbox.focus();
+	}
+	
+	@Listen("onBlur=#textbox")
+	public void doEdited(){
+		label.setValue(textbox.getValue());
+		textbox.setVisible(false);
+		label.setVisible(true);
+		Events.postEvent("onEdited", this, null);
+	}
+	
+
+}
