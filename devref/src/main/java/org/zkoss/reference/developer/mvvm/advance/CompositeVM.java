2a574764e312d27f708ecbbcda3f9b58e7d35e29
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-09-26 10:07:54 +0000

    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@286 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- devref/src/main/java/org/zkoss/reference/developer/mvvm/advance/CompositeVM.java
+++ devref/src/main/java/org/zkoss/reference/developer/mvvm/advance/CompositeVM.java
@@ -1,30 +1,11 @@
 package org.zkoss.reference.developer.mvvm.advance;
 
 import org.zkoss.bind.annotation.Command;
-import org.zkoss.bind.annotation.NotifyChange;
 
 public class CompositeVM {
 
-	private String name = "John";
 	private String title = "My Title";
 	
-	
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	
-	
-
-	@Command
-	@NotifyChange("name")
-	public void reset() {
-		name = "Lin";
-	}
 	
 	@Command
 	public void print(){
