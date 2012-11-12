fcdbeb9775dd9acb9d4c2dd45f0f40cde068a2f4
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-11-12 04:08:42 +0000

    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@351 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- developersreference/integration.spring/src/main/java/org/zkoss/reference/developer/spring/composer/ResolverComposer.java
+++ developersreference/integration.spring/src/main/java/org/zkoss/reference/developer/spring/composer/ResolverComposer.java
@@ -1,6 +1,11 @@
 package org.zkoss.reference.developer.spring.composer;
 
+import org.zkoss.reference.developer.spring.domain.OrderService;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zkplus.spring.SpringUtil;
+import org.zkoss.zul.Label;
 import org.zkoss.zul.Window;
 
 /**
@@ -9,6 +14,16 @@ import org.zkoss.zul.Window;
  */
 public class ResolverComposer extends SelectorComposer<Window> {
 
-
+	@WireVariable("orderService")
+	private OrderService orderService;
+	
+	@Wire("#number")
+	private Label label;
+	
+	@Override
+	public void doAfterCompose(Window comp) throws Exception {
+		super.doAfterCompose(comp);
+		label.setValue(Integer.toString(orderService.list().size()));
+	}
 
 }
