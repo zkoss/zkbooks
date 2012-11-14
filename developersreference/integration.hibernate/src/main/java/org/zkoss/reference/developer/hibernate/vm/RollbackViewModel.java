836f622bdae395eda16611727e106bc4e783da86
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-11-14 10:09:50 +0000

    for rollback test
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@363 04b67794-27a6-3c7a-43d9-00c53d7466cb

---
+++ developersreference/integration.hibernate/src/main/java/org/zkoss/reference/developer/hibernate/vm/RollbackViewModel.java
@@ -0,0 +1,26 @@
+package org.zkoss.reference.developer.hibernate.vm;
+
+import org.zkoss.reference.developer.hibernate.domain.Order;
+
+/**
+ * 
+ * @author Hawk
+ *
+ */
+public class RollbackViewModel {
+
+
+	private Order newOrder;
+
+	public Order getNewOrder() {
+		return newOrder;
+	}
+
+	public void setNewOrder(Order newOrder) {
+		this.newOrder = newOrder;
+	}
+	
+	
+	
+
+}
