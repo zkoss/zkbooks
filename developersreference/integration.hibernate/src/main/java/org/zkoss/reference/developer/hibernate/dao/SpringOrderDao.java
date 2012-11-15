7f7f008e2ad967ed5cdfb410b6d3b77a63e4a76b
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-11-15 10:21:10 +0000

    refine spring based DAO example
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@370 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- developersreference/integration.hibernate/src/main/java/org/zkoss/reference/developer/hibernate/dao/SpringOrderDao.java
+++ developersreference/integration.hibernate/src/main/java/org/zkoss/reference/developer/hibernate/dao/SpringOrderDao.java
@@ -2,11 +2,15 @@ package org.zkoss.reference.developer.hibernate.dao;
 
 import java.util.List;
 
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.zkoss.reference.developer.hibernate.domain.Order;
 
 /**
@@ -15,23 +19,48 @@ import org.zkoss.reference.developer.hibernate.domain.Order;
 @Repository
 public class SpringOrderDao {
 
-	private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//	private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Transactional
 	public List<Order> findAll() {
-		Session session = sessionFactory.openSession();
+		Session session = sessionFactory.getCurrentSession();
 		Query query = session.createQuery("select o from Order as o");
-		List result = query.list();
-		session.close();
+		List<Order> result = query.list();
 		return result;
 	}
-	
-	public Order create(Order newOrder) {
-		
-		Session session = sessionFactory.openSession();
-		Transaction tx = session.beginTransaction();
+
+	/**
+	 * rollback is handled in filter.
+	 * @param newOrder
+	 * @return
+	 * @throws HibernateException
+	 */
+	public Order save(Order newOrder) throws HibernateException{
+		//FIXME handle rollback
+		Session session = sessionFactory.getCurrentSession();
 		session.save(newOrder);
-		tx.commit();
-		session.close();
 		return newOrder;
 	}
-
+	
+	public void errorSave(Order newOrder) throws HibernateException{
+		Session session = sessionFactory.getCurrentSession();
+		session.save(newOrder);
+		// throw exception to test
+		throw new HibernateException("error save");
+	}
+	/**
+	 * Initialize lazy-loaded collection.
+	 * @param order
+	 * @return
+	 */
+	public Order load(Order order){
+		//check to avoid initializing again
+		if (!Hibernate.isInitialized(order.getItems())){
+			order = (Order)sessionFactory.getCurrentSession().load(Order.class, order.getId());
+			Hibernate.initialize(order.getItems());
+		}
+		return order;
+	}
 }
