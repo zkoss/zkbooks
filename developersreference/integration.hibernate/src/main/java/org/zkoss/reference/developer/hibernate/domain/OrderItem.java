265fc7cb0d092d45e7435f9fd0a74d70c0eeda8a
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-12-03 04:10:52 +0000

    use generated hashcode and equals
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@421 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- developersreference/integration.hibernate/src/main/java/org/zkoss/reference/developer/hibernate/domain/OrderItem.java
+++ developersreference/integration.hibernate/src/main/java/org/zkoss/reference/developer/hibernate/domain/OrderItem.java
@@ -72,23 +72,29 @@ public class OrderItem {
 		this.quantity = quantity;
 	}
 
-//	public long getOrderId() {
-//		return orderId;
-//	}
-//
-//	public void setOrderId(long orderId) {
-//		this.orderId = orderId;
-//	}
-
 	@Override
 	public boolean equals(Object obj) {
-		return ((obj instanceof OrderItem) &&
-				 this.id.equals(((OrderItem)obj).getId()));
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof OrderItem))
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	@Override
 	public int hashCode() {
-		return id.hashCode();
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	public Order getOrder() {
