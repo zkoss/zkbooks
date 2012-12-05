199506e1dd356c443d51656dca01786a598434db
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-12-05 07:11:48 +0000

    *add richlet example * change image path to absolute for richlet
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@431 04b67794-27a6-3c7a-43d9-00c53d7466cb

---
+++ tutorial/src/tutorial/richlet/SearchRichlet.java
@@ -0,0 +1,193 @@
+package tutorial.richlet;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.zkoss.xel.VariableResolver;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.GenericRichlet;
+import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.RichletConfig;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.util.Composer;
+import org.zkoss.zk.ui.util.Template;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Hbox;
+import org.zkoss.zul.Image;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listhead;
+import org.zkoss.zul.Listheader;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Vbox;
+import org.zkoss.zul.Window;
+
+import tutorial.Car;
+import tutorial.CarService;
+import tutorial.CarServiceImpl;
+
+public class SearchRichlet extends GenericRichlet {
+
+	private CarService carService = new CarServiceImpl();
+
+	public void service(Page page) throws Exception {
+		Component rootComponent = buildUserInterface();
+		rootComponent.setPage(page);
+	}
+	
+	private Component buildUserInterface(){
+
+
+		//build search area
+		final Textbox keywordBox = new Textbox();
+		Button searchButton = new Button("Search");
+		searchButton.setImage("/img/search.png");
+		
+		Hbox searchArea = new Hbox();
+		searchArea.setAlign("center");
+		searchArea.appendChild(new Label("Keyword:"));
+		searchArea.appendChild(keywordBox);
+		searchArea.appendChild(searchButton);
+		
+		
+		//build Car List Area
+		Listhead listhead = new Listhead();
+		listhead.appendChild(new Listheader("Model"));
+		listhead.appendChild(new Listheader("Make"));
+		Listheader priceHeader = new Listheader("Price");
+		priceHeader.setWidth("20%");
+		listhead.appendChild(priceHeader);
+		final Listbox carListbox = new Listbox();
+		carListbox.setHeight("160px");
+		carListbox.setEmptyMessage("No car found in the result");
+		carListbox.setTemplate("model", new ListboxTemplate());
+		carListbox.appendChild(listhead);
+		
+		
+		//build Detail Area
+		final Label modelLabel = new Label();
+		final Label makeLabel = new Label();
+		final Label priceLabel = new Label();
+		final Label descriptionLabel = new Label();
+		Vbox vbox = new Vbox();
+		vbox.appendChild(modelLabel);
+		vbox.appendChild(makeLabel);
+		vbox.appendChild(priceLabel);
+		vbox.appendChild(descriptionLabel);
+		
+		final Image previewImage = new Image();
+		previewImage.setWidth("250px");
+		Hbox detailArea = new Hbox();
+		detailArea.setStyle("margin-top:20px");
+		detailArea.appendChild(previewImage);
+		detailArea.appendChild(vbox);
+		
+		//add event listeners
+		searchButton.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
+			//search
+			@Override
+			public void onEvent(Event event) throws Exception {
+				String keyword = keywordBox.getValue();
+				List<Car> result = carService.search(keyword);
+				carListbox.setModel(new ListModelList<Car>(result));
+			}
+			
+		});
+		
+		carListbox.addEventListener(Events.ON_SELECT, new EventListener<Event>() {
+			//show detail
+			@Override
+			public void onEvent(Event event) throws Exception {
+				Car selected = carListbox.getSelectedItem().getValue();
+				previewImage.setSrc(selected.getPreview());
+				modelLabel.setValue(selected.getModel());
+				makeLabel.setValue(selected.getMake());
+				priceLabel.setValue(selected.getPrice().toString());
+				descriptionLabel.setValue(selected.getDescription());
+			}
+		});
+		
+		Window window = new Window();
+		window.setTitle("Search");
+		window.setWidth("600px");
+		window.setBorder("normal");
+		window.appendChild(searchArea);
+		window.appendChild(carListbox);
+		window.appendChild(detailArea);
+		
+		return window;
+	}
+	
+
+	@Override
+	public void init(RichletConfig config) {
+		super.init(config);
+		//initialize resources
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		//destroy resources
+	}
+	
+//	@Listen("onClick = #searchButton")
+//	public void search(){
+//		String keyword = keywordBox.getValue();
+//		List<Car> result = carService.search(keyword);
+//		carListbox.setModel(new ListModelList<Car>(result));
+//	}
+	
+//	@Listen("onSelect = #carListbox")
+//	public void showDetail(){
+//		Car selected = carListbox.getSelectedItem().getValue();
+//		previewImage.setSrc(selected.getPreview());
+//		modelLabel.setValue(selected.getModel());
+//		makeLabel.setValue(selected.getMake());
+//		priceLabel.setValue(selected.getPrice().toString());
+//		descriptionLabel.setValue(selected.getDescription());
+//	}
+}
+
+class ListboxTemplate implements Template {
+
+	@SuppressWarnings("rawtypes")
+	public Component[] create(Component parent, Component insertBefore,
+			VariableResolver resolver, Composer composer){
+		
+		Car car = (Car)resolver.resolveVariable("each");
+		Listitem listitem = new Listitem();
+		listitem.appendChild(new Listcell(car.getModel()));
+		listitem.appendChild(new Listcell(car.getMake()));
+		Listcell priceCell = new Listcell();
+		priceCell.appendChild(new Label("$"));
+		priceCell.appendChild(new Label(car.getPrice().toString()));
+		listitem.appendChild(priceCell);
+		
+		 //append to the parent
+        if (insertBefore ==null){
+                parent.appendChild(listitem);
+        }else{
+                parent.insertBefore(listitem, insertBefore);
+        }
+        
+		Component[] components = new Component[1];
+		components [0] = listitem;
+
+		return components;
+	}
+
+	@Override
+	public Map<String, Object> getParameters() {
+		Map<String,Object> parameters = new HashMap<String, Object>();
+		//set  variable
+		parameters.put("var","each");
+		return null;
+	}
+}
