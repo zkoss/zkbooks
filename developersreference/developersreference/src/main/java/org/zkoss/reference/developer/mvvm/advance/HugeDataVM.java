c867cec562695d57df85304c816c45ed5bfcf2db
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-12-21 06:39:05 +0000

    remove unused Java list.
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@466 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvvm/advance/HugeDataVM.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvvm/advance/HugeDataVM.java
@@ -1,7 +1,5 @@
 package org.zkoss.reference.developer.mvvm.advance;
 
-import java.util.List;
-
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
@@ -13,14 +11,11 @@ public class HugeDataVM {
 
 	private PersonDao personDao = new PersonDao();
 	private LivePersonListModel personListModel = new LivePersonListModel(personDao);
-	private List<Person> personList;
 	private Person selectedPerson;
-	private int currentPage = 1;
 	
 	@Init
 	public void init(){
 		personDao.generateData();
-		personList = personDao.findAll();
 	}
 
 	@Command @NotifyChange({"personListModel","selectedPerson"})
@@ -48,14 +43,6 @@ public class HugeDataVM {
 	}
 	
 	
-	@Command @NotifyChange({"personList","selectedPerson"})
-	public void listDelete(){
-		int index = personList.indexOf(selectedPerson);
-		if (index != -1){
-			personList.remove(index);
-			selectedPerson = new Person();
-		}
-	}
 	public LivePersonListModel getPersonListModel() {
 		return personListModel;
 	}
@@ -70,22 +57,6 @@ public class HugeDataVM {
 
 	public void setSelectedPerson(Person selectedPerson) {
 		this.selectedPerson = selectedPerson;
-	}
-
-	public List<Person> getPersonList() {
-		return personList;
-	}
-
-	public void setPersonList(List<Person> personList) {
-		this.personList = personList;
-	}
-
-	public int getCurrentPage() {
-		return currentPage;
-	}
-
-	public void setCurrentPage(int currentPage) {
-		this.currentPage = currentPage;
 	}
 	
 }
