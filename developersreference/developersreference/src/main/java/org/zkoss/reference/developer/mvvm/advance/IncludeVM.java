b2fc451ca040a8ba262c3b6ec1c45135b17df7fd
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2012-12-24 02:47:01 +0000

    add "passing arguments to include" example
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@468 04b67794-27a6-3c7a-43d9-00c53d7466cb

---
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvvm/advance/IncludeVM.java
@@ -0,0 +1,23 @@
+package org.zkoss.reference.developer.mvvm.advance;
+
+
+public class IncludeVM {
+
+	private String zul = "inner.zul";
+	private String myArgument = "vm-argument-value";
+	
+	public String getInnerZul() {
+		return zul;
+	}
+	public void setInnerZul(String zul) {
+		this.zul = zul;
+	}
+	public String getMyArgument() {
+		return myArgument;
+	}
+	public void setMyArgument(String myArgument) {
+		this.myArgument = myArgument;
+	}
+	
+
+}
