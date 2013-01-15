8705d5229a40dc242be98d690922b8879f710d99
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2013-01-15 01:16:25 +0000

    add "EL expression" example
    git-svn-id: https://zkbooks.googlecode.com/svn/trunk@495 04b67794-27a6-3c7a-43d9-00c53d7466cb

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvvm/databinding/InitVM.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvvm/databinding/InitVM.java
@@ -5,8 +5,18 @@ import org.zkoss.bind.annotation.Init;
 
 public class InitVM {
 
+	private String value="my-test-value";
+	
 	@Init
 	public void init(@BindingParam("arg1") String arg1){
 		System.out.println("arg1: "+arg1);
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 }
