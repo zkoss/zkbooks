f4043e4d538804f86c8b2158927e24b4f202f4ed
hawkchen@potix.com <hawkchen@potix.com@04b67794-27a6-3c7a-43d9-00c53d7466cb>
2013-08-23 01:36:11 +0000

    branch for 3.0.0.RC release
    git-svn-id: https://zkbooks.googlecode.com/svn/tags/zssessentials@605 04b67794-27a6-3c7a-43d9-00c53d7466cb

---
+++ 3.0.0.RC/src/main/java/org/zkoss/zss/essential/advanced/MyBeanResolver.java
@@ -0,0 +1,17 @@
+package org.zkoss.zss.essential.advanced;
+
+import org.zkoss.xel.VariableResolver;
+import org.zkoss.xel.XelException;
+
+/**
+ * A example of VariableResolver to get JavaBeans
+ * @author Hawk
+ *
+ */
+public class MyBeanResolver implements VariableResolver {
+	
+	@Override
+	public Object resolveVariable(String name) throws XelException {
+		return MyBeanService.getMyBeanService().get(name);
+	}
+}
