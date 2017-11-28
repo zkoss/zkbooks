480933efe40a677b170dc22d8738593bbf9f447c
hawkchen <hawkchen@potix.com>
2017-11-28 18:29:16 +0800

    fix group model example

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/model/Food.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/model/Food.java
@@ -1,29 +1,69 @@
 package org.zkoss.reference.developer.mvc.model;
 
 public class Food {
-    String _category,  _name, _nutrients;
-    int _percentageOfDaily, _calories;
+    private String category;
+    private String name;
+    private String topNutrients;
+    private Integer dailyPercent;
+    private Integer calories;
+    private String quantity;
 
-    public Food(String cat, String nm, String nutr, int pod, int cal) {
-        _category = cat;
-        _name = nm;
-        _nutrients = nutr;
-        _percentageOfDaily = pod;
-        _calories = cal;
+    public Food(String category, String name, String topNutrients,
+                Integer dailyPercent, Integer calories, String quantity) {
+        this.category = category;
+        this.name = name;
+        this.topNutrients = topNutrients;
+        this.dailyPercent = dailyPercent;
+        this.calories = calories;
+        this.quantity = quantity;
     }
+
     public String getCategory() {
-        return _category;
+        return category;
     }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public String getName() {
-        return _name;
+        return name;
     }
-    public String getNutrients() {
-        return _nutrients;
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public int getPercentageOfDaily() {
-        return _percentageOfDaily;
+
+    public String getTopNutrients() {
+        return topNutrients;
     }
-    public int getCalories() {
-        return _calories;
+
+    public void setTopNutrients(String topNutrients) {
+        this.topNutrients = topNutrients;
     }
+
+    public Integer getDailyPercent() {
+        return dailyPercent;
+    }
+
+    public void setDailyPercent(Integer dailyPercent) {
+        this.dailyPercent = dailyPercent;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
 }
--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/model/FoodGroupsModel.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/model/FoodGroupsModel.java
@@ -3,10 +3,18 @@ package org.zkoss.reference.developer.mvc.model;
 import org.zkoss.zul.FieldComparator;
 import org.zkoss.zul.GroupsModelArray;
 
+import java.util.List;
+
 public class FoodGroupsModel extends GroupsModelArray {
     public FoodGroupsModel(Food[][] foods) {
         super(foods, new FieldComparator("category", true));
     }
+
+    public FoodGroupsModel(List<Food> foods) {
+        super(foods.toArray(new Food[0]), new FieldComparator("category", true));
+    }
+
+
     protected Object createGroupHead(Object[] groupdata,int index,int col) {
         return new Object[] {groupdata[0], new Integer(col)};
     }
