6bfe2a8cc208f323a34a8267cb5d39fac1f1ee85
hawkchen <hawkchen@potix.com>
2018-05-22 16:00:07 +0800

    a new example: desktop cleanup listener for VM detached

--- mvvmreference/src/main/java/org/zkoss/mvvm/databinding/InitVM.java
+++ mvvmreference/src/main/java/org/zkoss/mvvm/databinding/InitVM.java
@@ -1,31 +1,38 @@
 package org.zkoss.mvvm.databinding;
 
-import org.zkoss.bind.annotation.BindingParam;
-import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.*;
+import org.zkoss.zk.ui.Desktop;
+import org.zkoss.zk.ui.util.DesktopCleanup;
 
 public class InitVM {
 
-	private String value="my-test-value";
-	private int price= 14;
-	
-	@Init
-	public void init(@BindingParam("arg1") String arg1){
-		System.out.println("arg1: "+arg1);
-	}
+    private String value = "my-test-value";
+    private int price = 14;
 
-	public String getValue() {
-		return value;
-	}
+    @Init
+    public void init(@BindingParam("arg1") String arg1, @ContextParam(ContextType.DESKTOP)Desktop desktop) {
+        System.out.println("init: arg1: " + arg1);
+        desktop.addListener(new DesktopCleanup() {
+            @Override
+            public void cleanup(Desktop desktop) throws Exception {
+                System.out.println(desktop.getId() + " is destroyed");
+            }
+        });
+    }
 
-	public void setValue(String value) {
-		this.value = value;
-	}
+    public String getValue() {
+        return value;
+    }
 
-	public int getPrice() {
-		return price;
-	}
+    public void setValue(String value) {
+        this.value = value;
+    }
 
-	public void setPrice(int price) {
-		this.price = price;
-	}
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
