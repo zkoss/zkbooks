ffe09522f4b56300ea7213048d08d35d6eb6d766
hawkchen <hawkchen@potix.com>
2018-08-23 13:45:21 +0800

    refine Initiator implementation

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/uipattern/AllCustomerFinder.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/uipattern/AllCustomerFinder.java
@@ -2,25 +2,27 @@ package org.zkoss.reference.developer.uipattern;
 
 import org.zkoss.zk.ui.*;
 import org.zkoss.zk.ui.util.*;
-
 import java.util.Map;
 
 public class AllCustomerFinder implements Initiator, InitiatorExt {
 
+    @Override
     public void doInit(Page page, Map args) throws Exception {
         String name = (String)args.get("name");
         page.setAttribute(name != null ? name: "customers", CustomerManager.findAll());
-        Executions.getCurrent().setAttribute("e", "in execution");
     }
 
     @Override
-    public void doAfterCompose(Page page, Component[] comps) throws Exception {
+    public void doAfterCompose(Page page, Component[] comps) throws Exception{
 
     }
 
+    @Override
     public boolean doCatch(Throwable ex) throws Exception { //nothing to do
         return false;
     }
+
+    @Override
     public void doFinally() throws Exception { //nothing to do
     }
 }
