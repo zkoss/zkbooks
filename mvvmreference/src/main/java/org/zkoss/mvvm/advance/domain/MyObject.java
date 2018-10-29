8da3a9292f394074dd162132b9ed6d9a3a3c8baa
Rudy Huang <rudyhuang@potix.com>
2018-10-29 12:41:15 +0800

    add navigation model example

---
+++ mvvmreference/src/main/java/org/zkoss/mvvm/advance/domain/MyObject.java
@@ -0,0 +1,27 @@
+package org.zkoss.mvvm.advance.domain;
+
+public class MyObject {
+    private String name;
+    private String description;
+
+    public MyObject(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
