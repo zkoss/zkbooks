1f6d38002de1fe9eb35702201d9cc523d80beab4
hawkchen <hawkchen@potix.com>
2018-12-21 12:13:35 +0800

    add error handling examples

---
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/uipattern/ErrorHandlingComposer.java
@@ -0,0 +1,29 @@
+package org.zkoss.reference.developer.uipattern;
+
+import org.slf4j.*;
+import org.zkoss.zk.ui.*;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+
+import java.util.Map;
+
+public class ErrorHandlingComposer extends SelectorComposer<Component> {
+
+    private static final Logger logger = LoggerFactory.getLogger(ErrorHandlingComposer.class);
+
+    @WireVariable
+    private Map<String, Object> requestScope;
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+
+        //via execution.getAttribute()
+        Execution execution = Executions.getCurrent();
+        Exception ex1 = (Exception) execution.getAttribute("javax.servlet.error.exception");
+
+        //via requestScope map
+        Exception ex2 = (Exception) requestScope.get("javax.servlet.error.exception");
+
+        logger.error("from " + getPage().getRequestPath());
+    }
+}
