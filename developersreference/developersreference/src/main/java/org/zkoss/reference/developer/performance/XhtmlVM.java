f3d3e9c868d74845bbbb39cbe4f6a3ccb9f57718
hawkchen <hawkchen@potix.com>
2019-04-11 11:39:31 +0800

    add xhtml example

---
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/performance/XhtmlVM.java
@@ -0,0 +1,22 @@
+package org.zkoss.reference.developer.performance;
+
+public class XhtmlVM {
+    private String title = "please fill your name";
+    private String name = "your name";
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
