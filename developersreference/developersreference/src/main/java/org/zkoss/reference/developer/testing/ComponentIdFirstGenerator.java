ef5be76b616d7b232155edaa8327d833a6cb3c9c
hawkchen <hawkchen@potix.com>
2019-04-16 10:22:25 +0800

    refactor - ComponentIdFirstGenerator

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/testing/ComponentIdFirstGenerator.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/testing/ComponentIdFirstGenerator.java
@@ -1,14 +1,17 @@
 package org.zkoss.reference.developer.testing;
 
 import org.zkoss.zk.ui.*;
-import org.zkoss.zk.ui.impl.StaticIdGenerator;
+import org.zkoss.zk.ui.impl.*;
 import org.zkoss.zk.ui.metainfo.*;
 import org.zkoss.zk.ui.sys.IdGenerator;
 
 /**
  * Generate an UUID upon a component's ID first if exists. If not, then use static ID generator.
  * With this generator, you should specify id on those components you need to verify their state during testing.<br/>
- * Unlike {@link StaticIdGenerator}, component creation order can't affect this generator.
+ * Benefit:<br/>
+ * Unlike {@link StaticIdGenerator}, component creation order can't affect this generator.<br/>
+ * Limitation: <br/>
+ * It can't handle same ID under the different space owner.
  * @author hawk
  */
 public class ComponentIdFirstGenerator implements IdGenerator {
@@ -18,21 +21,37 @@ public class ComponentIdFirstGenerator implements IdGenerator {
 	public String nextComponentUuid(Desktop desktop, Component comp,
                                     ComponentInfo compInfo) {
 		String id = comp.getId();
-		if (id.isEmpty()){ //find id in properties
-			if (compInfo != null && compInfo.getProperties() != null){
-				for (Property p :compInfo.getProperties()){
-					if ("id".equals(p.getName())){
-						id = p.getValue(comp).toString();
-						break;
-					}
-				}
-			}
+		if (id.isEmpty()){
+			id = getIdFromComponentInfo(comp, compInfo);
 		}
 		if (id.isEmpty()){
 			return staticIdGenerator.nextComponentUuid(desktop, comp, compInfo);
 		}else{
 			return id;
 		}
+	}
+
+	protected String getIdFromComponentInfo(Component comp, ComponentInfo compInfo) {
+		String id = "";
+
+		if (compInfo != null && compInfo.getProperties() != null){
+			for (Property p :compInfo.getProperties()){
+				if ("id".equals(p.getName())){
+					id = p.getValue(comp).toString();
+					break;
+				}
+			}
+		}
+		return id;
+	}
+
+	protected String getSpaceOwnerId(Component comp){
+		String id = "";
+		if (comp.getSpaceOwner() instanceof  Component){
+			Component spaceOwner = (Component) comp.getSpaceOwner();
+			id = spaceOwner.getId();
+		}
+		return id;
 	}
 
 	@Override
