0edec85b69d51914f8b9dbb8e42fb49fdca03f34
hawkchen <hawkchen@potix.com>
2019-07-30 11:57:21 +0800

    add select by listmodel

---
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/model/listmodel/SelectByListModelComposer.java
@@ -0,0 +1,40 @@
+package org.zkoss.reference.developer.mvc.model.listmodel;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zul.*;
+import org.zkoss.zul.ext.*;
+
+import java.util.*;
+
+/**
+ * select/deselect a listitem via ListModel API.
+ * @author Hawk Chen
+ */
+public class SelectByListModelComposer extends SelectorComposer<Component> {
+
+    @Wire("listbox")
+    private Listbox listbox;
+    @Wire("intbox")
+    private Intbox intbox;
+
+    private ListModelList<Locale> listModel = new ListModelList<>(Locale.getAvailableLocales());
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        listModel.setMultiple(true);
+        listbox.setModel(listModel);
+    }
+
+    @Listen("onClick = button")
+    public void toggle(){
+        Locale locale = listModel.getElementAt(intbox.getValue());
+        if (listModel.getSelection().contains(locale)){
+            listModel.removeFromSelection(locale);
+        }else{
+            listModel.addToSelection(locale);
+        }
+    }
+}
