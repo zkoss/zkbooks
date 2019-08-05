f75ab1d57cc0e066cb3e62d147468723dca5c7f3
hawkchen <hawkchen@potix.com>
2019-08-05 18:06:26 +0800

    refine the example of ctrlKeys

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/uipattern/KeystrokeComposer.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/uipattern/KeystrokeComposer.java
@@ -1,9 +1,11 @@
 package org.zkoss.reference.developer.uipattern;
 
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.KeyEvent;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
 import org.zkoss.zul.Textbox;
+
 
 public class KeystrokeComposer extends SelectorComposer<Component> {
 
@@ -22,5 +24,13 @@ public class KeystrokeComposer extends SelectorComposer<Component> {
     public void onCancel() {
         username.setValue("");
         password.setValue("");
+    }
+
+    @Listen("onCtrlKey = div")
+    public void handleKeys(KeyEvent event) {
+        System.out.println(event.getKeyCode()
+                + ",ctrl pressed: " + event.isCtrlKey()
+                + ",alt pressed: " + event.isAltKey()
+                + ", shift pressed:" + event.isShiftKey());
     }
 }
