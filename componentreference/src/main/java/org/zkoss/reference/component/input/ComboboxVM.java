8b1358668c87e0829475c58b3dcc5d6500755dd9
hawkchen <hawkchen@potix.com>
2020-01-13 09:53:29 +0800

    combobox - value and selectedItem

---
+++ componentreference/src/main/java/org/zkoss/reference/component/input/ComboboxVM.java
@@ -0,0 +1,22 @@
+package org.zkoss.reference.component.input;
+
+public class ComboboxVM {
+    private String inputValue;
+    private String selectedValue;
+
+    public String getInputValue() {
+        return inputValue;
+    }
+
+    public void setInputValue(String inputValue) {
+        this.inputValue = inputValue;
+    }
+
+    public String getSelectedValue() {
+        return selectedValue;
+    }
+
+    public void setSelectedValue(String selectedValue) {
+        this.selectedValue = selectedValue;
+    }
+}
