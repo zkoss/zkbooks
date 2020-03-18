8dfde6ae783cee8dd394c2523092f371970c62cd
hawkchen <hawkchen@potix.com>
2020-03-18 09:29:41 +0800

    scroll tree into view

--- componentreference/src/main/java/org/zkoss/reference/component/data/tree/BinaryPackageController.java
+++ componentreference/src/main/java/org/zkoss/reference/component/data/tree/BinaryPackageController.java
@@ -2,13 +2,29 @@ package org.zkoss.reference.component.data.tree;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.*;
 
 public class BinaryPackageController extends SelectorComposer<Component> {
 
+	@Wire
+	private Tree tree;
+	private DefaultTreeModel<PackageData> treeModel;
+
+	int[] path = {1,1}; //target node to scroll into view
 	private static final long serialVersionUID = 43014628867656917L;
-	
-	public TreeModel<TreeNode<PackageData>> getTreeModel() {
-		return new DefaultTreeModel<PackageData>(PackageDataUtil.getRoot());
+
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		treeModel = new DefaultTreeModel<PackageData>(PackageDataUtil.getRoot());
+		tree.setModel(treeModel);
+	}
+
+	@Listen("onClick = #show")
+	public void scrollIntoView(){
+		treeModel.setOpenObjects(treeModel.getRoot().getChildren());
+		treeModel.addToSelection(treeModel.getChild(path));
 	}
 }
