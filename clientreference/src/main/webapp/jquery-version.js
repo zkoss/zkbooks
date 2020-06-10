cc4160b3a54d13613faa08132c7db7d5fda76edc
hawkchen <hawkchen@potix.com>
2020-06-10 15:55:30 +0800

    minor

--- clientreference/src/main/webapp/jquery-version.js
+++ clientreference/src/main/webapp/jquery-version.js
@@ -1,2 +1,2 @@
-console.log('jq.fn.jquery: ' +jq.fn.jquery);
-console.log('$.fn.jquery: '+ $.fn.jquery);
+console.log('jq.fn.jquery: ' +jq.fn.jquery); // jQuery bundled with ZK
+console.log('$.fn.jquery: '+ $.fn.jquery); // another jQuery if loaded
