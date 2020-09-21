463b8ddee0f7d9692fcf2950c7a802e11b446e86
hawkchen <hawkchen@potix.com>
2020-09-21 15:54:50 +0800

    add hellow view model example

---
+++ mvvmreference/src/main/java/org/zkoss/mvvm/viewmodel/HelloVM.java
@@ -0,0 +1,13 @@
+package org.zkoss.mvvm.viewmodel;
+
+public class HelloVM {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
