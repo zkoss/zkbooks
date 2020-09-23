6f9e44a42d7b4fb0770bb65b33c6d13d4c5cda28
hawkchen <hawkchen@potix.com>
2020-09-23 17:35:43 +0800

    add data binding syntax sugar example

---
+++ mvvmreference/src/main/java/org/zkoss/mvvm/parameters/ParametersVM.java
@@ -0,0 +1,35 @@
+package org.zkoss.mvvm.parameters;
+
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zul.ListModelList;
+
+import java.util.Arrays;
+
+public class ParametersVM {
+    private ListModelList items = new ListModelList();
+    private String message;
+
+    public ParametersVM() {
+        String[] names = {"A", "B", "C", "D"};
+        items.addAll(Arrays.asList(names));
+    }
+
+    @NotifyChange("message")
+    public void delete(String item){
+        items.remove(item);
+        message = "remove item: " + item;
+    }
+
+    @NotifyChange("message")
+    public void showIndex(int index){
+        message = "item index: " + index;
+    }
+
+    public ListModelList getItems() {
+        return items;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
