588d83fa28cf047eb03d293daac65e6a2d429b76
hawkchen <hawkchen@potix.com>
2020-09-30 15:17:40 +0800

    call another viewmodel's command example

---
+++ mvvmreference/src/main/java/org/zkoss/mvvm/databinding/CommandVM.java
@@ -0,0 +1,10 @@
+package org.zkoss.mvvm.databinding;
+
+import org.zkoss.zk.ui.util.Notification;
+
+public class CommandVM {
+
+    public void hello(){
+        Notification.show("hello");
+    }
+}
