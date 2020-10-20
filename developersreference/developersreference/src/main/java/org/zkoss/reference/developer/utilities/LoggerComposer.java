516e88841268317840c73c15b200ebed63e01f04
hawkchen <hawkchen@potix.com>
2020-10-20 09:30:28 +0800

    * include log4j dependency for 9.5.0 * add example logging code

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/utilities/LoggerComposer.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/utilities/LoggerComposer.java
@@ -2,7 +2,9 @@ package org.zkoss.reference.developer.utilities;
 
 import org.slf4j.*;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
 
 public class LoggerComposer extends SelectorComposer {
     Logger logger = LoggerFactory.getLogger(LoggerComposer.class);
@@ -12,5 +14,11 @@ public class LoggerComposer extends SelectorComposer {
         super.doAfterCompose(comp);
         logger.debug("this is debug message");
         logger.info("this is info message", comp);
+        logger.warn("Warning message!", comp);
+    }
+
+    @Listen(Events.ON_CLICK + "=button")
+    public void printError(){
+        logger.error("Error message!", new RuntimeException("fake error"));
     }
 }
