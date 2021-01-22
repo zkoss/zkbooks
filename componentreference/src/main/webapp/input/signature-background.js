076e1589197d2805207b41bdf6101e76814e9308
hawkchen <hawkchen@potix.com>
2021-01-22 14:37:55 +0800

    update signature examples

--- componentreference/src/main/webapp/input/signature-background.js
+++ componentreference/src/main/webapp/input/signature-background.js
@@ -1,5 +1,9 @@
+/** draw an image in a canvas of the signature
+ * https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
+ */
 function initBackground(){
-    var canvas = zk.Widget.$('$sig').$n('canvas');
+    var canvas = zk.Widget.$('$sig') //get zk widget by ID selector
+        .$n('canvas'); // get its inner canvas element
     var context = canvas.getContext('2d');
     var img = new Image();
     img.src = '/component/multimedia/zklogo3.png';
