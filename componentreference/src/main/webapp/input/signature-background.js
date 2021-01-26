47e2af06be5025b195280979e739569fde531d0d
hawkchen <hawkchen@potix.com>
2021-01-26 17:24:26 +0800

    update signature background example

--- componentreference/src/main/webapp/input/signature-background.js
+++ componentreference/src/main/webapp/input/signature-background.js
@@ -1,13 +1,30 @@
 /** draw an image in a canvas of the signature
  * https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
  */
-function initBackground(){
-    var canvas = zk.Widget.$('$sig') //get zk widget by ID selector
-        .$n('canvas'); // get its inner canvas element
-    var context = canvas.getContext('2d');
-    var img = new Image();
-    img.src = '/component/multimedia/zklogo3.png';
-    img.onload = function(){
-        context.drawImage(img, 0, 0);
-    }
-}
+zk.afterLoad('zkmax.wgt', function() {
+    var exWidget = {};
+    zk.override(zkmax.wgt.Signature.prototype, exWidget, {
+		bind_: function(){
+			exWidget.bind_.apply(this, arguments);
+            this.drawBackgroundImage();
+		},
+		undo: function(){
+		    exWidget.undo.apply(this, arguments);
+            this.drawBackgroundImage();
+		},
+		clear: function(){
+		    exWidget.clear.apply(this, arguments);
+            this.drawBackgroundImage();
+		},
+        drawBackgroundImage: function(){
+            var signatureWidget = this;
+            var img = new Image();
+            img.src = '/component/multimedia/zklogo3.png';
+            img.onload = function(){
+                var context = signatureWidget.$n('canvas').getContext('2d');
+                context.drawImage(img, 0, 0);
+            }
+        }
+    });
+
+});
