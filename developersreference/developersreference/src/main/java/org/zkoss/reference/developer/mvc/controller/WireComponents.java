a3876f101fb252dd86d9df8b08b1b16dbf25bbf5
hawkchen <hawkchen@potix.com>
2021-11-16 11:06:17 +0800

    wire a components inside <include>

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/controller/WireComponents.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/controller/WireComponents.java
@@ -3,15 +3,21 @@ package org.zkoss.reference.developer.mvc.controller;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.*;
 import org.zkoss.zuti.zul.*;
 
-import java.awt.*;
 
 public class WireComponents extends SelectorComposer {
     @Wire("::shadow forEach")
-    ForEach forEach;
+    private ForEach forEach;
     @Wire(":host if")
-    If ifComponent;
+    private If ifComponent;
+
+    @Wire
+    private Label message;
+
+    @Wire("include #message") //because Include create an ID space, or #messageArea #message
+    private Label message2;
 
     String[] fruits = {"apple", "banana", "orange"};
 
@@ -22,5 +28,8 @@ public class WireComponents extends SelectorComposer {
         forEach.setItems(fruits);
         forEach.recreate();
         System.out.println(ifComponent);
+
+        System.out.format("#message: %s\n", message);
+        System.out.format("#include #message: %s\n", message2);
     }
 }
