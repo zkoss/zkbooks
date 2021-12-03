bf33eda9a44c8ecf45615e0fb1c76a035f6c1187
hawkchen <hawkchen@potix.com>
2021-12-03 12:19:16 +0800

    ckeditor - enable /removePlugin context menu of

--- componentreference/src/main/webapp/input/config.js
+++ componentreference/src/main/webapp/input/config.js
@@ -5,4 +5,9 @@ CKEDITOR.editorConfig = function(config) {
     config.scayt_autoStartup = true;
     //locale
     config.language = 'de';
+    // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html#cfg-removePlugins
+    // A plugin required by another plugin cannot be removed and will cause an error to be thrown. So for example if contextmenu is required by tabletools, it can only be removed if tabletools is not loaded
+//    config.removePlugins='contextmenu, tabletools, tableselection, liststyle';
+    //need to right-click on the space area out of text elements in the content
+//    config.enableContextMenu = false;
 };
