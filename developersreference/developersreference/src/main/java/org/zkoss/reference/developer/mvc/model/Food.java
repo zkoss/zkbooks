61117e6937b28cf273250c1fdabe8c8794ed143d
hawkchen <hawkchen@potix.com>
2022-09-28 12:18:31 +0800

    refactor. move sample data

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/model/Food.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/model/Food.java
@@ -1,6 +1,35 @@
 package org.zkoss.reference.developer.mvc.model;
 
 public class Food {
+    //Data
+    static public Object[][] foods = new Object[][] { //Note: the order does not matter
+        new Object[] { "Vegetables", "Asparagus", "Vitamin K", 115, 43},
+        new Object[] { "Vegetables", "Beets", "Folate", 33, 74},
+        new Object[] { "Vegetables", "Bell peppers", "Vitamin C", 291, 24},
+        new Object[] { "Vegetables", "Cauliflower", "Vitamin C", 92, 28},
+        new Object[] { "Vegetables", "Eggplant", "Dietary Fiber", 10, 27},
+        new Object[] { "Vegetables", "Onions", "Chromium", 21, 60},
+        new Object[] { "Vegetables", "Potatoes", "Vitamin C", 26, 132},
+        new Object[] { "Vegetables", "Spinach", "Vitamin K", 1110, 41},
+        new Object[] { "Vegetables", "Tomatoes", "Vitamin C", 57, 37},
+        new Object[] { "Seafood", "Salmon", "Tryptophan", 103, 261},
+        new Object[] { "Seafood", "Shrimp", "Tryptophan", 103, 112},
+        new Object[] { "Seafood", "Scallops", "Tryptophan", 81, 151},
+        new Object[] { "Seafood", "Cod", "Tryptophan", 90, 119},
+        new Object[] { "Fruits", "Apples", "Manganese", 33, 61},
+        new Object[] { "Fruits", "Cantaloupe", "Vitamin C", 112, 56},
+        new Object[] { "Fruits", "Grapes", "Manganese", 33, 61},
+        new Object[] { "Fruits", "Pineapple", "Manganese", 128, 75},
+        new Object[] { "Fruits", "Strawberries", "Vitamin C", 24, 48},
+        new Object[] { "Fruits", "Watermelon", "Vitamin C", 24, 48},
+        new Object[] { "Poultry & Lean Meats", "Beef, lean organic", "Tryptophan", 112, 240},
+        new Object[] { "Poultry & Lean Meats", "Lamb", "Tryptophan", 109, 229},
+        new Object[] { "Poultry & Lean Meats", "Chicken", "Tryptophan", 121, 223},
+        new Object[] { "Poultry & Lean Meats", "Venison ", "Protein", 69, 179},
+        new Object[] { "Grains", "Corn ", "Vatamin B1", 24, 177},
+        new Object[] { "Grains", "Oats ", "Manganese", 69, 147},
+        new Object[] { "Grains", "Barley ", "Dietary Fiber", 54, 270}
+    };
     private String category;
     private String name;
     private String topNutrients;
