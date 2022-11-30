cc961e1fea7db55da7e37be1df4415a6ffda1929
hawkchen <hawkchen@potix.com>
2022-11-30 11:10:18 +0800

    chosen filtering example

---
+++ componentreference/src/main/java/org/zkoss/reference/component/input/ChosenboxFilteringComposer.java
@@ -0,0 +1,37 @@
+package org.zkoss.reference.component.input;
+
+import org.zkoss.zk.ui.util.Composer;
+import org.zkoss.zkmax.zul.Chosenbox;
+import org.zkoss.zul.*;
+
+import java.util.*;
+
+/**
+ * Show available items when an item contains the user input.
+ */
+public class ChosenboxFilteringComposer implements Composer<Chosenbox> {
+
+
+    @Override
+    public void doAfterCompose(Chosenbox chosenbox) throws Exception {
+        ListModelList<Locale> fullModel = new ListModelList(Locale.getAvailableLocales());
+        ListModel model = ListModels.toListSubModel(fullModel, new Comparator() {
+            @Override
+            public int compare(Object typedValue, Object modelObject) {
+
+                String TypedValueAsString = (String) typedValue;
+                Locale modelObjectAsLocale = (Locale) modelObject;
+
+                boolean containsTypedString = modelObjectAsLocale.toString().toLowerCase().contains(TypedValueAsString.toLowerCase());
+
+                //return 0: show object
+                //return !0: don't show object
+
+                return containsTypedString?0:1;
+            }
+        }, 10);
+        chosenbox.setModel(model);
+
+    }
+
+}
