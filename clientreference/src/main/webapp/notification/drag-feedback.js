f7bd8130fc0b0e9bc9d636119f22dd617aeca8d2
hawkchen <hawkchen@potix.com>
2023-04-28 15:49:18 +0800

    add dragging feedback example

---
+++ clientreference/src/main/webapp/notification/drag-feedback.js
@@ -0,0 +1,15 @@
+/**
+ * Purpose: provide a visual feedback when dragging a label. See zk.DnD.ghost()
+ * Based on version: 9.6.3
+ */
+zk.afterLoad('zul.wgt', function() {
+    var exWidget = {};
+    zk.override(zul.wgt.Label.prototype, exWidget, {
+        /** return a string and zk.DnD.ghost() encloses it with an icon.
+        return null and zk.DnD.ghost() just clones the widget's DOM elements.
+        */
+        getDragMessage_: function(){
+            return this.getValue();
+        }
+    });
+});
