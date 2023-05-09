cfc54675c88532ed4d776e110adc9064cecaebbe
hawkchen <hawkchen@potix.com>
2023-05-09 14:27:12 +0800

    escape xml in ItemRenderer

--- componentreference/src/main/java/org/zkoss/reference/component/input/TooltipRenderer.java
+++ componentreference/src/main/java/org/zkoss/reference/component/input/TooltipRenderer.java
@@ -1,11 +1,13 @@
 package org.zkoss.reference.component.input;
 
+import org.zkoss.xml.XMLs;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zul.ItemRenderer;
 
-public class TooltipRenderer implements ItemRenderer {
+public class  TooltipRenderer implements ItemRenderer {
     @Override
     public String render(Component owner, Object data, int index) throws Exception {
+        data = XMLs.escapeXML("<" + data + ">");
         return String.format("<span title=\"%s\" style=\"width: 100%%;display: inline-block;\">%s</span>", data, data);
     }
 }
