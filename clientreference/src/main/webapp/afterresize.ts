c6f54e851d43474ba2e400503e1675c76ca8075a
hawkchen <hawkchen@potix.com>
2023-08-21 18:19:48 +0800

    add jquery examples

--- clientreference/src/main/webapp/afterresize.ts
+++ clientreference/src/main/webapp/afterresize.ts
@@ -1,9 +1,8 @@
-"use strict";
 /**
 * Adds a function that will be executed after all onSize events are fired.
 * ZK invokes the added function once and removes it.
 * It's suitable for doing things after all widgets render completely.
 */
-zk.afterResize(function () {
+zk.afterResize(function() {
     print();
 });
--- clientreference/src/main/webapp/jquery-version.js
+++ clientreference/src/main/webapp/jquery-version.js
@@ -1,2 +1,3 @@
-console.log('jq.fn.jquery: ' +jq.fn.jquery); // jQuery bundled with ZK
-console.log('$.fn.jquery: '+ $.fn.jquery); // another jQuery if loaded
+"use strict";
+console.log('jq.fn.jquery: ' + jq.fn.jquery); // jQuery bundled with ZK
+console.log('$.fn.jquery: ' + $.fn.jquery); // another jQuery if loaded
