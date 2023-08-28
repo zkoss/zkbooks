6af0ae36dd1c156e3aa99738a2be7ff8dd0d9fab
hawkchen <hawkchen@potix.com>
2023-08-28 16:58:04 +0800

    update ts and config ignore js

--- clientreference/src/main/webapp/widgetCustomization/patch.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-zk.afterLoad('zul.wnd', function () {
-    const oldPanel = zk.augment(zul.wnd.Panel.prototype, {
-        onClose() {
-            alert('Modified zul.wnd.Panel.prototype.onClose');
-            return oldPanel.onClose();
-        }
-    });
-});
