1fbd5a332f0ede94ae7c22e55f362180eac58e75
hawkchen <hawkchen@potix.com>
2023-08-28 18:42:30 +0800

    add the way to skip a function in za11y

--- clientreference/src/main/webapp/widgetCustomization/portallayout-disable-nav.ts
+++ clientreference/src/main/webapp/widgetCustomization/portallayout-disable-nav.ts
@@ -2,13 +2,13 @@
  * Purpose: Avoid opening a popup when pressing a spacebar.
  * Since this feature is provided by za11y, we should load this js after za11y is loaded.
  */
-/* wait for https://tracker.zkoss.org/browse/ZK-5534
 zk.afterLoad('za11y, zkmax.layout', function() {
     const exWidget = zk.augment(zkmax.layout.Portallayout.prototype, {
-        doKeyDown_: function () {
-            // to skip the function in za11y, call its parent's parent's method
-            exWidget.doKeyDown_.apply(arguments);
+        doKeyDown_: function (...args: any[]) {
+            // the function below is overridden by za11y which shows the portal popup
+            // exWidget.doKeyDown_.apply(this, arguments);
+            // to skip the function in za11y, call the function before being overridden by za11y
+            this.$supers(zkmax.layout.Portallayout, 'doKeyDown_', args);
         },
     });
 });
-*/
