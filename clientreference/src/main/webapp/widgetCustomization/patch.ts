85103e7950e4bc11b3a4af1381e88d6bc3464d36
hawkchen <hawkchen@potix.com>
2023-08-29 17:50:41 +0800

    minor

--- clientreference/src/main/webapp/widgetCustomization/patch.ts
+++ clientreference/src/main/webapp/widgetCustomization/patch.ts
@@ -1,15 +1,15 @@
 
 zk.afterLoad('zul.wnd',
     function(){
+        //locate the target function in the target class (or parent class)
         const oldPanel = zk.augment(zul.wnd.Panel.prototype, {
+            //copy the function signatre
             onClose(): void {
-                alert('Modified zul.wnd.Panel.prototype.onClose');
-                
-                return oldPanel.onClose(); //call the original function before overidden
+                //implement the custom logic
+                zk.log('Modified zul.wnd.Panel.prototype.onClose');
+                //call the original function before overidden if it's needed
+                oldPanel.onClose.apply(this);
             },
         })
-});
-
-
-
-
+    }
+);
