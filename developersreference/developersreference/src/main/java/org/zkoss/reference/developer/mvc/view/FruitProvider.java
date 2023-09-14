91767b35ec96f09268ade0f2639fa0a66ac0aba9
hawkchen <hawkchen@potix.com>
2023-09-14 12:38:54 +0800

    add new tab in a tabbox example

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/view/FruitProvider.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/mvc/view/FruitProvider.java
@@ -1,16 +1,37 @@
 package org.zkoss.reference.developer.mvc.view;
 
-import org.zkoss.zul.ListModelArray;
+import org.zkoss.bind.annotation.*;
+import org.zkoss.zul.*;
 
-public class FruitProvider extends org.zkoss.zk.ui.select.SelectorComposer {
-    public ListModelArray fruits = new ListModelArray(
-            new String[][] {
-                    {"Apple", "10kg"},
-                    {"Orange", "20kg"},
-                    {"Mango", "12kg"}
-            });
+public class FruitProvider {
+    public ListModelList fruits = new ListModelList();
 
-    public ListModelArray getFruits() {
+    @Init
+    public void init() {
+        TabInfo tabinfo1 = new TabInfo();
+        tabinfo1.setName("Apple");
+        tabinfo1.setName("z-icon-user");
+
+        TabInfo tabPlus = new TabInfo();
+        tabPlus.setName("");
+        tabPlus.setIcon("z-icon-plus");
+        tabPlus.setCommand("add");
+        fruits.add(tabinfo1);
+        fruits.add(tabPlus);
+    }
+
+
+    @Command
+    public void add(){
+        TabInfo newTab = new TabInfo();
+        newTab.setName("new " + fruits.size());
+
+        fruits.add(fruits.size()-1, newTab); //keep the last tab
+        fruits.addToSelection(newTab);
+    }
+
+
+    public ListModelList getFruits() {
         return fruits;
     }
 }
