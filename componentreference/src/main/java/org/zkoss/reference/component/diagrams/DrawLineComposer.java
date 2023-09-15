162dc11c48a10eb8c62ce746c4f7a0de26f2394c
hawkchen <hawkchen@potix.com>
2023-09-15 17:33:11 +0800

    * add gpolygon example * add click to draw poly lines example

---
+++ componentreference/src/main/java/org/zkoss/reference/component/diagrams/DrawLineComposer.java
@@ -0,0 +1,42 @@
+package org.zkoss.reference.component.diagrams;
+
+import org.zkoss.gmaps.*;
+import org.zkoss.gmaps.event.MapMouseEvent;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.*;
+
+import java.util.*;
+
+public class DrawLineComposer extends SelectorComposer {
+    @Wire
+    private Gmaps map4draw;
+
+    private List<LatLng> clickedPoints = new LinkedList();
+
+    private Gpolyline currentPolyline = new Gpolyline();
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        map4draw.appendChild(currentPolyline);
+    }
+
+    @Listen("onMapClick = #map4draw")
+    public void recordDraw(MapMouseEvent event){
+        clickedPoints.add(event.getLatLng());
+        drawPolylines();
+    }
+
+    @Listen("onClick = #clear")
+    public void clearPoints(){
+        clickedPoints.clear();
+        currentPolyline.setPath(new LinkedList<>(clickedPoints));
+    }
+
+    private void drawPolylines() {
+        if (clickedPoints.size() > 1) {
+            currentPolyline.setPath(new LinkedList<>(clickedPoints));
+        }
+    }
+}
