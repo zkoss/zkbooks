64f42e34787e9cc2469daaf915e55078b9c75fb8
hawkchen <hawkchen@potix.com>
2023-09-20 10:36:33 +0800

    update tsconfig.json

--- clientreference/src/main/webapp/customization/drag-feedback.js
+++ clientreference/src/main/webapp/customization/drag-feedback.js
@@ -4,7 +4,7 @@
  * Based on version: 9.6.3
  */
 zk.afterLoad('zul.wgt', function () {
-    var exWidget = zk.augment(zul.wgt.Label.prototype, {
+    const exWidget = zk.augment(zul.wgt.Label.prototype, {
         /** return a string and zk.DnD.ghost() encloses it with an icon (plus or ban).
         return null and zk.DnD.ghost() just clones the widget's DOM elements.
         */
