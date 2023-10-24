c069df54a9e29e8808b390590730e0ed2d77fb87
hawkchen <hawkchen@potix.com>
2023-10-24 18:23:00 +0800

    fix cropper mvvm example add another barcode example

--- componentreference/src/main/java/org/zkoss/reference/component/multimedia/CropperVM.java
+++ componentreference/src/main/java/org/zkoss/reference/component/multimedia/CropperVM.java
@@ -2,11 +2,24 @@ package org.zkoss.reference.component.multimedia;
 
 import org.zkoss.bind.annotation.*;
 import org.zkoss.util.media.Media;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zkmax.zul.Cropper;
 
 public class CropperVM {
 
-    @Command
+    private Media media;
+
+    @Command @NotifyChange("media")
     public void crop(@BindingParam("image") Media media){
-        System.out.println(media.getFormat());
+        this.media = media;
+    }
+
+    @Command
+    public void changeSelection(@ContextParam(ContextType.TRIGGER_EVENT) Event event){
+        ((Cropper)event.getTarget()).crop();
+    }
+
+    public Media getMedia() {
+        return media;
     }
 }
