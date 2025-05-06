269973c8ad0f5c6b3ad41bf92a5edd91befb51b4
hawkhero <hawkhero@gmail.com>
2025-05-06 18:00:04 +0800

    show animation names

--- developersreference/developersreference/src/main/java/org/zkoss/reference/developer/integration/FontAwesomeDemoVM.java
+++ developersreference/developersreference/src/main/java/org/zkoss/reference/developer/integration/FontAwesomeDemoVM.java
@@ -5,57 +5,81 @@ import java.util.*;
 
 public class FontAwesomeDemoVM {
 
+    public static final String FONT_AWESOME_FONT_PATH_JAR = "web/zul/less/font/";
     private List<String> iconCssNames = new LinkedList<>();
     private List<String> brandIconCssNames = new LinkedList<>();
+    private List<String> animationNames = new LinkedList<>();
 
     public FontAwesomeDemoVM() {
         try {
             extractIconNames();
+            extractAnimationNames();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     static final String iconPrefix = "z-icon-";
+
     /**
      * Extracts icon names from the font-awesome.txt file
      */
-private void extractIconNames() throws IOException {
-    boolean isBrandSection = false;
-    boolean insideSection = false;
+    private void extractIconNames() throws IOException {
+        boolean isBrandSection = false;
+        boolean insideSection = false;
 
-    try (InputStream input = getClass().getClassLoader().getResourceAsStream("web/zul/less/font/_variables.less");
-         BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"))) {
-        String line;
-        while ((line = reader.readLine()) != null) {
-            String trimmedLine = line.trim();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(FONT_AWESOME_FONT_PATH_JAR + "_variables.less");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String trimmedLine = line.trim();
 
-            if (trimmedLine.equals(".fa-icons() {")) {
-                isBrandSection = false;
-                insideSection = true;
-                continue;
-            } else if (trimmedLine.equals(".fa-brand-icons() {")) {
-                isBrandSection = true;
-                insideSection = true;
-                continue;
-            } else if (trimmedLine.equals("}")) {
-                insideSection = false;
-                continue;
-            }
+                if (trimmedLine.equals(".fa-icons() {")) {
+                    isBrandSection = false;
+                    insideSection = true;
+                    continue;
+                } else if (trimmedLine.equals(".fa-brand-icons() {")) {
+                    isBrandSection = true;
+                    insideSection = true;
+                    continue;
+                } else if (trimmedLine.equals("}")) {
+                    insideSection = false;
+                    continue;
+                }
 
-            if (insideSection && line.contains(":")) {
-                String iconName = line.substring(0, line.indexOf(":")).trim();
-                if (!iconName.isEmpty()) {
-                    if (isBrandSection) {
-                        brandIconCssNames.add(iconPrefix + iconName);
-                    } else {
-                        iconCssNames.add(iconPrefix + iconName);
+                if (insideSection && line.contains(":")) {
+                    String iconName = line.substring(0, line.indexOf(":")).trim();
+                    if (!iconName.isEmpty()) {
+                        if (isBrandSection) {
+                            brandIconCssNames.add(iconPrefix + iconName);
+                        } else {
+                            iconCssNames.add(iconPrefix + iconName);
+                        }
                     }
                 }
             }
         }
     }
-}
+
+    private void extractAnimationNames() throws IOException {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(FONT_AWESOME_FONT_PATH_JAR + "_animated.less");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith(".@{fa-css-prefix}-")) {
+                    String animName;
+                    if (line.endsWith(",")) {
+                        animName = line.substring(".@{fa-css-prefix}-".length(), line.indexOf(",")).trim();
+                    } else {
+                        animName = line.substring(".@{fa-css-prefix}-".length(), line.indexOf(" {")).trim();
+                    }
+                    if (!animName.contains("@") && !animationNames.contains(iconPrefix + animName)) {
+                        animationNames.add(iconPrefix + animName);
+                    }
+                }
+            }
+        }
+    }
 
     public List<String> getIconCssNames() {
         return iconCssNames;
@@ -63,5 +87,9 @@ private void extractIconNames() throws IOException {
 
     public List<String> getBrandIconCssNames() {
         return brandIconCssNames;
+    }
+
+    public List<String> getAnimationNames() {
+        return animationNames;
     }
 }
